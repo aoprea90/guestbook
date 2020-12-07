071e57be29fefce8ee76e962a890ed71c57c3011
aoprea <alin.oprea@gfm-research.ro>
2020-12-07 14:14:38 +0200

    Add event listener for comment and form

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Conference;
-use App\Repository\CommentRepository;
-use App\Repository\ConferenceRepository;
+use App\Form\CommentFormType;
+use App\Repository\CommentRepository;;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,23 +18,24 @@ class ConferenceController extends AbstractController
 {
     private $twig;
 
-    public function __construct(Environment $twig)
+    private $entityManager;
+
+    public function __construct(Environment $twig, EntityManagerInterface $entityManager)
     {
         $this->twig = $twig;
+        $this->entityManager = $entityManager;
     }
 
     /**
      * @Route("/conference", name="homepage")
      */
-    public function index(ConferenceRepository $conferenceRepository): Response
+    public function index(): Response
     {
-        return new Response($this->twig->render('conference/index.html.twig', [
-            'conferences' => $conferenceRepository->findAll(),
-        ]));
+        return new Response($this->twig->render('conference/index.html.twig', []));
     }
 
     /**
-     * @Route("/conference/{id}", name="conference")
+     * @Route("/conference/{slug}", name="conference")
      * @param CommentRepository $commentRepository
      * @param Conference $conference
      * @return Response
@@ -39,8 +43,33 @@ class ConferenceController extends AbstractController
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
      */
-    public function show(Request $request, CommentRepository $commentRepository, Conference $conference) : Response
+    public function show(Request $request, CommentRepository $commentRepository, Conference $conference, string $photoDir) : Response
     {
+        $comment = new Comment();
+        $form = $this->createForm(CommentFormType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setConference($conference);
+
+            if ($photo = $form['photoFilename']->getData()) {
+                $fileName = bin2hex(random_bytes(6)).'.'.$photo->guessExtension();
+
+                try{
+                    $photo->move($photoDir, $fileName);
+                } catch (FileException $e) {
+                    // unable to upload the photo, give up
+                }
+
+                $comment->setPhotoFilename($fileName);
+            }
+
+            $this->entityManager->persist($comment);
+            $this->entityManager->flush();
+
+            $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
+        }
+
         $offset = max(0,$request->query->getInt('offset', 0));
         $paginator = $commentRepository->getConferenceComments($conference, $offset);
 
@@ -50,6 +79,7 @@ class ConferenceController extends AbstractController
              'comments' => $paginator,
              'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
              'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+             'comment_form' => $form->createView()
         ]));
     }
 
--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -4,9 +4,12 @@ namespace App\Entity;
 
 use App\Repository\CommentRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints\Date;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=CommentRepository::class)
+ * @ORM\HasLifecycleCallbacks()
  */
 class Comment
 {
@@ -19,16 +22,20 @@ class Comment
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
      */
     private $author;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank
      */
     private $text;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     * @Assert\Email
      */
     private $email;
 
@@ -94,11 +101,12 @@ class Comment
         return $this->createdAt;
     }
 
-    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    /**
+     * @ORM\PrePersist()
+     */
+    public function setCreatedAt($createdAt)
     {
-        $this->createdAt = $createdAt;
-
-        return $this;
+        $this->createdAt = new \DateTime();
     }
 
     public function getConference(): ?Conference
